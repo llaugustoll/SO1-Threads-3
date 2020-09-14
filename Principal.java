c5ee05403b7cec48c5b9d103669944ca973fae88
llaugustoll <53905902+llaugustoll@users.noreply.github.com>
2020-09-14 15:53:35 -0300

    Iniciando

---
+++ Principal.java
@@ -0,0 +1,10 @@
+package view;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int mat[][] = new int[3][5];
+		
+	}
+
+}
---
+++ Thread.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class Thread {
+	
+	private int mThread;
+	
+	public Thread(int mThread){
+		this.mThread = mThread;
+	}
+}
